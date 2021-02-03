79961a2db56d914e0ff18e293d36f52281f4a1a4
SangyeonCho <rien1026@gmail.com>
2021-02-03 10:41:04 +0900

    - [add]: mp4 type

--- src/ImageHandler.ts
+++ src/ImageHandler.ts
@@ -20,27 +20,48 @@ export const imageHandler = async (event: any, context: Context) => {
 		}
 
 		let fileType = file.contentType;
-		if (!fileType.includes('jpg') && !fileType.includes('jpeg') && !fileType.includes('png')) {
-			return { statusCode: 400, body: JSON.stringify({ msg: 'The file format must be jpg, jpeg, png.' }) };
+		if (!fileType.includes('jpg') && !fileType.includes('jpeg') && !fileType.includes('png') && !fileType.includes('gif')&& !fileType.includes('mp4')) {
+			return { statusCode: 400, body: JSON.stringify({ msg: 'The file format must be jpg, jpeg, png, gif, mp4.' }) };
 		}
 
+		let ts = ''+new Date().getSeconds() + new Date().getMilliseconds();
+		if(fileType.includes('mp4')){
+			await s3
+			.upload({
+				Bucket: process.env.BUCKET,
+				Body: file.content,
+				Key: 'images/video/' + ts + file.filename,
+				ACL: 'public-read',
+				ContentType: fileType,
+			})
+			.promise();
+
+			return {
+				headers: {
+					'Access-Control-Allow-Headers': 'Content-Type',
+					'Access-Control-Allow-Origin': '*',
+					'Access-Control-Allow-Methods': 'OPTIONS,POST',
+				},
+				statusCode: 200,
+				body: JSON.stringify({ msg: 'OK', data: { link: 'http://image.amipure.com/video/' + ts + file.filename } }),
+			};
+		}
+		
 		let file426;
 		let file720;
 		let file1280;
 		let metadata = await sharp(file.content).metadata();
-		file426 = await sharp(file.content).resize({ width: 426 }).toBuffer();
+		file426 = file.content;
 		file720 = file.content;
 		file1280 = file.content;
-	
 
-		if (metadata.width > 720) {
+		if(!fileType.includes('gif')){
+			file426 = await sharp(file.content).resize({ width: 426 }).toBuffer();
+		} else if (!fileType.includes('gif') && metadata.width > 720) {
 			file720 = await sharp(file.content).resize({ width: 720 }).toBuffer();
-		}
-
-		if (metadata.width > 1280) {
+		} else if (!fileType.includes('gif') && metadata.width > 1280) {
 			file1280 = await sharp(file.content).resize({ width: 1280 }).toBuffer();
 		}
-		let ts = new Date().getSeconds() + new Date().getMilliseconds();
 
 		await s3
 			.upload({
